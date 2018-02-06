22c5c4d673e54e1811c13069f54ab4b1ec49b523
logon.firstclass@gmail.com <logon.firstclass@gmail.com>
2018-02-06 16:27:55 +0700

    remove less than zero in sql

--- models/stock_adj.go
+++ models/stock_adj.go
@@ -41,10 +41,10 @@ func GetAllTransToProcessAvg(productID int) (num int64, PreAllStockAdj []PreAllS
 						select receive.created_at ,product_id as product_i_d ,0 as balance_qty,qty,price,average_cost,receive."flag",doc_type,receive_sub.i_d,'receive_sub' as tb   
 						from receive_sub join receive on receive_sub.doc_no = receive.doc_no where receive.active and  receive_sub.active and product_id = ? 
 				union all 
-						select "order".created_at ,product_id  ,0 as balance_qty,qty*-1 as qty,price,average_cost,"order"."flag",doc_type,order_sub.i_d,'order_sub' as tb   
+						select "order".created_at ,product_id  ,0 as balance_qty,qty,price,average_cost,"order"."flag",doc_type,order_sub.i_d,'order_sub' as tb   
 						from order_sub join "order" on order_sub.doc_no = "order".doc_no where "order".active and  order_sub.active and product_id = ?
 				union all 
-						select "pick_up".created_at ,product_id as product_i_d ,0 as balance_qty ,qty *-1 as qty,price,average_cost,"pick_up"."flag",doc_type,pick_up_sub.i_d,'pick_up_sub' as tb   
+						select "pick_up".created_at ,product_id as product_i_d ,0 as balance_qty ,qty,price,average_cost,"pick_up"."flag",doc_type,pick_up_sub.i_d,'pick_up_sub' as tb   
 						from pick_up_sub join "pick_up" on pick_up_sub.doc_no = "pick_up".doc_no where "pick_up".active and  pick_up_sub.active and product_id = ?  
 				union all 
 						select "stock_count".created_at ,product_id as product_i_d ,balance_qty,qty,price,average_cost,"stock_count"."flag",doc_type,stock_count_sub.i_d,'stock_count_sub' as tb   
@@ -89,11 +89,11 @@ func CalAllAvgAndUpdatDocTrans(productID int) (err error) {
 			}
 			_, err = o.Raw("update "+val.Tb+" set average_cost = ? where i_d = ?", gAverageCost, val.ID).Exec()
 		case 2:
-			gQty = gQty + val.Qty
+			gQty = gQty - val.Qty
 			val.AverageCost = gAverageCost
 			_, err = o.Raw("update "+val.Tb+" set average_cost = ? where i_d = ?", gAverageCost, val.ID).Exec()
 		case 3:
-			gQty = gQty + val.Qty
+			gQty = gQty - val.Qty
 			val.AverageCost = gAverageCost
 			_, err = o.Raw("update "+val.Tb+" set average_cost = ? where i_d = ?", gAverageCost, val.ID).Exec()
 		case 4:
