7d04568b9609709e2962d49231fa6e2daa9a9fb1
roengrit moonchai <logon.firstclass@gmail.com>
2018-02-12 18:00:01 +0700

    cal avg after save doc

--- models/stock_adj.go
+++ models/stock_adj.go
@@ -38,17 +38,17 @@ func init() {
 //GetAllTransToProcessAvg _
 func GetAllTransToProcessAvg(productID int) (num int64, PreAllStockAdj []PreAllStockAdj, err error) {
 	var sql = `select all_table.* ,product.product_type from (
-						select receive.created_at ,product_id as product_i_d ,0 as balance_qty,qty,price,average_cost,receive."flag",doc_type,receive_sub.i_d,'receive_sub' as tb   
-						from receive_sub join receive on receive_sub.doc_no = receive.doc_no where receive.active and  receive_sub.active and product_id = ? 
-				union all 
-						select "order".created_at ,product_id  ,0 as balance_qty,qty,price,average_cost,"order"."flag",doc_type,order_sub.i_d,'order_sub' as tb   
+						select receive.created_at ,product_id as product_i_d ,0 as balance_qty,qty,price,average_cost,receive."flag",doc_type,receive_sub.i_d,'receive_sub' as tb
+						from receive_sub join receive on receive_sub.doc_no = receive.doc_no where receive.active and  receive_sub.active and product_id = ?
+				union all
+						select "order".created_at ,product_id  ,0 as balance_qty,qty,price,average_cost,"order"."flag",doc_type,order_sub.i_d,'order_sub' as tb
 						from order_sub join "order" on order_sub.doc_no = "order".doc_no where "order".active and  order_sub.active and product_id = ?
-				union all 
-						select "pick_up".created_at ,product_id as product_i_d ,0 as balance_qty ,qty,price,average_cost,"pick_up"."flag",doc_type,pick_up_sub.i_d,'pick_up_sub' as tb   
-						from pick_up_sub join "pick_up" on pick_up_sub.doc_no = "pick_up".doc_no where "pick_up".active and  pick_up_sub.active and product_id = ?  
-				union all 
-						select "stock_count".created_at ,product_id as product_i_d ,balance_qty,qty,price,average_cost,"stock_count"."flag",doc_type,stock_count_sub.i_d,'stock_count_sub' as tb   
-						from stock_count_sub join "stock_count" on stock_count_sub.doc_no = "stock_count".doc_no where "stock_count".active and  stock_count_sub.active and stock_count.flag_temp = 0 and product_id = ? 
+				union all
+						select "pick_up".created_at ,product_id as product_i_d ,0 as balance_qty ,qty,price,average_cost,"pick_up"."flag",doc_type,pick_up_sub.i_d,'pick_up_sub' as tb
+						from pick_up_sub join "pick_up" on pick_up_sub.doc_no = "pick_up".doc_no where "pick_up".active and  pick_up_sub.active and product_id = ?
+				union all
+						select "stock_count".created_at ,product_id as product_i_d ,balance_qty,qty,price,average_cost,"stock_count"."flag",doc_type,stock_count_sub.i_d,'stock_count_sub' as tb
+						from stock_count_sub join "stock_count" on stock_count_sub.doc_no = "stock_count".doc_no where "stock_count".active and  stock_count_sub.active and stock_count.flag_temp = 0 and product_id = ?
 				) as all_table JOIN product on all_table.product_i_d = product.i_d
 				ORDER BY all_table.created_at,doc_type,all_table.i_d`
 	sql = strings.Replace(sql, "?", strconv.Itoa(productID), -1)
@@ -169,7 +169,7 @@ func CalAllAvg() {
 	if len(*StockAdj) > 0 {
 		return
 	}
-	qs.Filter("flag", 0).Limit(5).RelatedSel().All(StockAdj)
+	qs.Filter("flag", 0).Limit(1000).RelatedSel().All(StockAdj)
 	if len(*StockAdj) >= 1 {
 		for _, val := range *StockAdj {
 			_, _ = o.Raw("update stock_adj set flag = 1 where product_id = ?", val.Product.ID).Exec()
